ea322993af0aaf59e30a380430d95e24f0c95110
tkzc00 <2238983030@qq.com>
2024-02-02 19:13:22 +0800

    更新user.sql

--- user-center-backend/sql/user.sql
+++ user-center-backend/sql/user.sql
@@ -18,3 +18,30 @@ create table user
     planetCode   varchar(512)                       null comment '星球编号'
 )
     comment '用户表';
+
+create table team
+(
+    id          bigint auto_increment comment 'id' primary key,
+    name        varchar(256)                       not null comment '队伍名称',
+    description varchar(1024)                      null comment '描述',
+    maxNum      int      default 1                 not null comment '最大人数',
+    expireTime  datetime                           null comment '过期时间',
+    userId      bigint comment '用户ID',
+    status      int      default 0                 not null comment '0-公开，1-私有，2-加密',
+    password    varchar(512)                       null comment '密码',
+    createTime  datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    updateTime  datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete    tinyint  default 0                 not null comment '是否删除'
+)
+    comment '队伍表';
+
+create table user_team
+(
+    id         bigint auto_increment comment 'id' primary key,
+    userId     bigint comment '用户ID',
+    teamId     bigint comment '队伍ID',
+    joinTime   datetime                           null comment '加入时间',
+    createTime datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    updateTime datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete   tinyint  default 0                 not null comment '是否删除'
+) comment '用户队伍关联表';
